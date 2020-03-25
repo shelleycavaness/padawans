2752451932efc7c8e495996438d433495bb04620
shelley cavaness <shelleycavaness@yahoo.com>
2020-03-25 18:00:47 +0100

    added src/setup/ that i forgot

--- api/src/setup/router.js
+++ api/src/setup/router.js
@@ -1,7 +1,7 @@
 import express from "express";
 // import projectRoutes from "../modules/projects/routes";
 // import pageRoutes from"../modules/pages/routes";
-// import userRoutes from "../modules/users/routes";
+import padawanRoutes from "../modules/padawans/routes";
 
 const Router = app => {
   let apiRoutes = express.Router();
@@ -13,6 +13,8 @@ const Router = app => {
 
   //Project router
   app.use("/api/", apiRoutes);
+  app.use("/api/padawans", padawanRoutes);
+
   // app.use("/api/projects", projectRoutes);
   // app.use("/api/pages", pageRoutes);
   // app.use("/api/users", userRoutes);
