151f08b067e487a4c12605de0b42aeb97f3f3e4c
rimIlmia <rim.chakroun.pro@gmail.com>
2020-03-26 01:57:43 +0100

    requete preparées

--- api/src/modules/skills/query.js
+++ api/src/modules/skills/query.js
@@ -11,18 +11,18 @@ const Query = {
     });
   },
   getById: id => {
-    let sqlQuery = `SELECT modules.module_name, skills.skill_name FROM skills INNER JOIN modules ON skills.modules_id = modules.id WHERE modules.id=${id}`;
+    let sqlQuery = `SELECT modules.module_name, skills.skill_name FROM skills,modules WHERE skills.modules_id = modules.id AND skills.id=?`;
     return new Promise((resolve, reject) => {
-      db.query(sqlQuery, (err, rows) => {
+      db.query(sqlQuery, [id], (err, rows) => {
         if (err) reject(err);
         resolve(rows);
       });
     });
   },
   getByModule: id => {
-    let sqlQuery = `SELECT * FROM skills WHERE modules_id= ${id}`;
+    let sqlQuery = "SELECT * FROM skills WHERE modules_id=?";
     return new Promise((resolve, reject) => {
-      db.query(sqlQuery, (err, rows) => {
+      db.query(sqlQuery, [id], (err, rows) => {
         if (err) reject(err);
         resolve(rows);
       });
