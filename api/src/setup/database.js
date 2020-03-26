889a9d3146f33e47e75def52ce12117dc5d3226b
KK <kula.jega@gmail.com>
2020-03-26 10:48:12 +0100

    Postman

--- api/src/setup/database.js
+++ api/src/setup/database.js
@@ -2,18 +2,19 @@ import mysql from "mysql2";
 import databaseConfig from "../config/database";
 
 const Connection = mysql.createConnection({
-  host: databaseConfig.development.host,
-  user: databaseConfig.development.username,
-  password: databaseConfig.development.password,
-  database: databaseConfig.development.database
+	host: databaseConfig.development.host,
+	port: databaseConfig.development.port,
+	user: databaseConfig.development.username,
+	password: databaseConfig.development.password,
+	database: databaseConfig.development.database
 });
 //Connecting to database
 Connection.connect(err => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
 
-  console.log("connected as id " + Connection.threadId);
+	console.log("connected as id " + Connection.threadId);
 });
 export default Connection;
