87c36cf0dddf4c8d3ca29375e4c05f64cc16794e
KK <kula.jega@gmail.com>
2020-04-27 14:44:52 +0200

    Router

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+
 const App = () => {
-	return <h1>Hello world</h1>;
+	return (
+		<Router>
+			<div>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/login">Login</Link>
+					</li>
+				</ul>
+				<hr />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/login">
+						<Login />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 };
 export default App;
