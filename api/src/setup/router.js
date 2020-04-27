a64b3802aab2963c48d8e8f58acf48d960227d5d
KK <kula.jega@gmail.com>
2020-04-27 12:45:39 +0200

    Skill-tree-front

--- api/src/setup/router.js
+++ api/src/setup/router.js
@@ -6,18 +6,18 @@ import levelRoutes from "../modules/levels/routes";
 import descriptionRoutes from "../modules/descriptions/routes";
 
 const Router = app => {
-  let apiRoutes = express.Router();
+	let apiRoutes = express.Router();
 
-  // Home route. We'll end up changing this to our main front end index later.
-  app.get("/", function(req, res) {
-    res.send("This Route is not yet defined.");
-  });
-  //Project router
-  app.use("/api/", apiRoutes);
-  app.use("/api/padawans", padawanRoutes);
-  app.use("/api/skills", skillRoutes);
-  app.use("/api/levels", levelRoutes);
-  app.use("/api/descriptions", descriptionRoutes);
-  app.use("/api/modules", moduleRoutes);
+	// Home route. We'll end up changing this to our main front end index later.
+	app.get("/", function(req, res) {
+		res.send("This Route is not yet defined.");
+	});
+	//Project router
+	app.use("/api/", apiRoutes);
+	app.use("/api/padawans", padawanRoutes);
+	app.use("/api/skills", skillRoutes);
+	app.use("/api/levels", levelRoutes);
+	app.use("/api/descriptions", descriptionRoutes);
+	app.use("/api/modules", moduleRoutes);
 };
 export default Router;
--- api/src/setup/start-server.js
+++ api/src/setup/start-server.js
@@ -1,15 +1,15 @@
 // Start server
 
-const StartServer = (server) => {
-    console.info('SETUP - Starting server..')
+const StartServer = server => {
+	console.info("SETUP - Starting server..");
 
-    server.listen(3010, (error) => {
-        if (error) {
-            console.error('ERROR - Unable to start server.')
-        } else {
-            console.info(`INFO - Server started on http://localhost:${3010} [DEV]`)
-        }
-    })
-}
+	server.listen(3009, error => {
+		if (error) {
+			console.error("ERROR - Unable to start server.");
+		} else {
+			console.info(`INFO - Server started on http://localhost:${3009} [DEV]`);
+		}
+	});
+};
 
-export default StartServer
+export default StartServer;
