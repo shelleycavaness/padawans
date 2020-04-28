47625dd5e1f58ffa77950050f9c9b7d379343e55
rimIlmia <rim.chakroun.pro@gmail.com>
2020-04-28 11:40:22 +0200

    update

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -32418,7 +32418,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55968" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59397" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
