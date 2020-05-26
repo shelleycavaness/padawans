842332142c646ae92313e707fbf53d1f55e3773f
rimIlmia <rim.chakroun.pro@gmail.com>
2020-05-26 14:27:55 +0200

    modules skills

--- api/src/modules/modules/service.js
+++ api/src/modules/modules/service.js
@@ -14,15 +14,25 @@ const ModuleService = {
       }));
   },
   getSkills: async () => {
-    let modulesWithSkills = await moduleQueries.getSkills();
-    let moduleReduce = modulesWithSkills.reduce((tab, element) => {
-      tab[element.id_modules] = [...(tab[element.id_modules] || []), element];
-      return tab;
-    }, {});
+    // let modulesWithSkills = await moduleQueries.getSkills();
+    // let moduleReduce = modulesWithSkills.reduce((tab, element) => {
+    //   tab[element.id_modules] = [...(tab[element.id_modules] || []), element];
+    //   return tab;
+    // }, {});
+    let modules = await moduleQueries.getAll();
+    let resultat = [];
+    for (let i = 0; i < modules.length; i++) {
+      let module = {
+        id_modules: modules[i].id_modules,
+        module_name: modules[i].module_name,
+      };
+      let skills = await moduleQueries.getByModule(modules[i].id_modules);
+      resultat.push({ ...module, skills });
+    }
 
     return {
       status: 200,
-      payload: { success: true, data: moduleReduce },
+      payload: { success: true, data: resultat },
     };
   },
 
