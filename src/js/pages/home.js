e6f24cac145afd7784be67bf0991a0d1177ba351
shelley cavaness <shelleycavaness@yahoo.com>
2020-05-27 12:11:05 +0200

    fixed /store/tree.js and added comments also updated /pages/home.js

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
-import api from "../utils/api";
-import Modules from "../components/modules/modules";
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {fetchModules} from '../store/tree'
 
-const Home = (props) => {
+import api from '../utils/api'
+
+const Home = () => {
+  const dispatch = useDispatch()
+  const moduleIsLoading = useSelector(state => state.tree.modules.isLoading)
+  const modulesCollection = useSelector(state => state.tree.modules.collection)
+
+  useEffect(() => {
+      dispatch(fetchModules())
+  }, [])
+
+  if (moduleIsLoading) return <span>Modules are loading</span>
+
   return (
-    <div className="container">
-      <h1>Home</h1>
-      <Modules />
-    </div>
-  );
-};
-export default Home;
+      <section>
+          <h1>Home</h1>
+          {
+              modulesCollection && modulesCollection.map(item => <span key={item.id}>{item.name}</span>)
+          }
+          <button onClick={() => dispatch({ type: 'CLEAR_MODULES' })}> Clear modules </button>
+      </section>
+  )
+}
+
+export default Home
