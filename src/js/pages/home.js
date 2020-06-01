6fb2ff0f1fb59cf9ea2174a3e06d5fd1051b3f71
shelley cavaness <shelleycavaness@yahoo.com>
2020-06-01 16:26:46 +0200

    worked shelley perso branch redux

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,13 +1,48 @@
-import React, { useState } from "react";
-import api from "../utils/api";
-import Modules from "../components/modules/modules";
 
-const Home = (props) => {
-  return (
-    <div className="container">
-      <h1>Home</h1>
-      <Modules />
-    </div>
-  );
-};
-export default Home;
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {fetchModules, fetchSkills} from '../store/tree'
+
+// import api from '../utils/api'
+
+const Home = () => {
+    const dispatch = useDispatch()
+    const moduleIsLoading = useSelector(state => state.tree.modules.isLoading)
+    const modulesCollection = useSelector(state => state.tree.modules.collection)
+    const skillCollection = useSelector(state => state.tree.skills.collection)
+
+    useEffect(() => {
+        dispatch(fetchModules()),
+        dispatch(fetchSkills())
+    }, [])
+
+    if (moduleIsLoading) return <span>Modules are loading</span>
+
+    return (
+        <section>
+            <h1>Home</h1>
+            { 
+                modulesCollection && modulesCollection.map(item => <p key={item.id_modules}>{item.module_name}</p>)
+            }
+            <br/>
+            <button onClick={() => dispatch({ type: 'CLEAR_MODULES' })}> Clear modules </button>
+
+        { 
+            skillCollection && skillCollection.map(item => <p>{ item.skill_name } </p>)  
+        }
+        </section>
+    )
+}
+
+export default Home
+
+
+ // useEffect(() => {
+    //     dispatch({ type: 'FETCH_MODULES' });
+    //     api
+    //         .get('/modules/')
+    //         .then(response => {
+    //             let result = response.data.data
+    //             dispatch({ type: 'SET_MODULES', payload: result });
+    //         })
+    // }, [])
--- src/js/store/tree.js
+++ src/js/store/tree.js
@@ -13,54 +13,87 @@ import { combineReducers } from 'redux'
 import api from '../utils/api'
 
 const name = ( state = null, action) =>{
-	switch (action.type) {
-		case 'SET_TREE_NAME' :
-			return action.payload
-		case 'Clear_TREE_NAME' :
-			return null	
-		default: 
-			return state
-	}
+  switch (action.type) {
+    case 'SET_TREE_NAME' :
+	  return action.payload
+	case 'Clear_TREE_NAME' :
+	  return null	
+	default: 
+	  return state
+  }
 }
 
 const modulesInitialState = {
-	collection: null,
-	isLoading: false
+  collection: null,
+  isLoading: false
+}
+
+const skillsInitialState={
+  collection:null,
+  isLoading: false	
 }
 
 export const fetchModules = () => {
-	return dispatch => {
-			dispatch({ type: 'FETCH_MODULES' });
-			api
-				.get('/modules/')
-				.then(response => {
-					let result = response.data.data
-					console.log('****************', result)
-					dispatch({ type: 'SET_MODULES', payload: result });
-				})
+  return dispatch => {
+    dispatch({ type: 'FETCH_MODULES' });
+	  api
+	    .get('/modules/')
+		.then(response => {
+		  let result = response.data.data
+		    console.log('****************', result)
+			dispatch({ type: 'SET_MODULES', payload: result });
+		})
 	}
 }
+export const fetchSkills = () => {
+	return dispatch => {
+	  dispatch({ type: 'FETCH_SKILLS' });
+		api
+		  .get('/skills/')
+		  .then(response => {
+			let result = response.data.data
+			  console.log('################### skills: ', result)
+			  dispatch({ type: 'SET_SKILLS', payload: result });
+		  })
+	  }
+  }
 
 const modules = (state = modulesInitialState, action) => {
-	switch (action.type) {
-			case "FETCH_MODULES":
-					return { ...state, isLoading: true }
-			case "SET_MODULES":
-					return { ...state, 
-						collection: action.payload, 
-						isLoading: false }
-			case "CLEAR_MODULES":
-					return modulesInitialState
-			default:
-					return state
-	}
+  switch (action.type) {
+    case "FETCH_MODULES":
+	  return { ...state, isLoading: true }
+	case "SET_MODULES":
+	  return { ...state, 
+		collection: action.payload, 
+		isLoading: false }
+	case "CLEAR_MODULES":
+	  return modulesInitialState
+	default:
+	  return state
+  }
 }
 
+const skills = (state = skillsInitialState, action) => {
+	switch (action.type) {
+	  case "FETCH_SKILLS":
+		return { ...state, isLoading: true }
+	  case "SET_SKILLS":
+		return { ...state, 
+		  collection: action.payload, 
+		  isLoading: false }
+	  case "CLEAR_SKILLS":
+		return skillsInitialState
+	  default:
+		return state
+	}
+  }
+  
 
 
 const treeReducer = combineReducers({
 	name,
 	modules,
+	skills,
 })
 
 export default treeReducer;
